dc986cbdf4c415d5c7109d66a826e9de92b6f09c
chaolun <0chaolun0@gmail.com>
2017-07-31 00:11:41 +0800

    - add new ui assets

---
+++ Assets/Scripts/Editor/TrapezoidDrawer.cs
@@ -0,0 +1,39 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor (typeof(Trapezoid))]
+public class TrapezoidDrawer : Editor
+{
+	public Trapezoid trapezoid;
+
+	void OnEnable ()
+	{
+		trapezoid = target as Trapezoid;
+		if (trapezoid.Mesh == null || trapezoid.Mesh.vertexCount != 8) {
+			trapezoid.Mesh = new Mesh ();
+			trapezoid.Mesh.vertices = new Vector3[] {
+				new Vector3 (-0.5f, -0.5f, -0.5f),
+				new Vector3 (0.5f, 0.5f, 0.5f),
+				new Vector3 (-0.5f, -0.5f, 0),
+				new Vector3 (-0.5f, 0, -0.5f),
+				new Vector3 (0, -0.5f, -0.5f),
+				new Vector3 (0.5f, 0.5f, 0),
+				new Vector3 (0.5f, 0, 0.5f),
+				new Vector3 (0, 0.5f, 0.5f),
+			};
+			trapezoid.Mesh.triangles = new int[3 * 8];
+		}
+	}
+
+	void OnSceneGUI ()
+	{
+		for (int i = 0; i < trapezoid.Mesh.vertexCount; i++) {
+			EditorGUI.BeginChangeCheck ();
+			var point = Handles.DoPositionHandle (trapezoid.transform.TransformPoint (trapezoid.Mesh.vertices [i]), trapezoid.transform.rotation);
+			if (EditorGUI.EndChangeCheck ()) {
+				trapezoid.Mesh.vertices [i] = trapezoid.transform.InverseTransformPoint (point); 
+				EditorUtility.SetDirty (trapezoid);
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/Utilities/Trapezoid.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+[RequireComponent (typeof(MeshFilter))]
+public class Trapezoid : MonoBehaviour
+{
+	protected MeshFilter meshFilter;
+
+	public Mesh Mesh {
+		get {
+			if (meshFilter == null) {
+				meshFilter = GetComponent<MeshFilter> () ?? gameObject.AddComponent<MeshFilter> ();
+			}
+			return meshFilter.sharedMesh;
+		}
+		set {
+			meshFilter.mesh = value;
+		}
+	}
+
+	void Start ()
+	{
+		
+	}
+}
