7f8814be74a7319d26b1c6bf523d0796534ebf38
chaolun <0chaolun0@gmail.com>
2017-08-12 13:42:39 +0800

    - optimize center on child

--- Assets/Scripts/Components/CenterOnChild.cs
+++ Assets/Scripts/Components/CenterOnChild.cs
@@ -47,7 +47,7 @@ public class CenterOnChild : MonoBehaviour,IBeginDragHandler,IEndDragHandler
 			EventTriggerListener.Get (child.gameObject).EndDrag = (eventData => {
 				scrollView.OnEndDrag (eventData);
 				if (center != null) {
-					centerCoroutines.Add (StartCoroutine (CenterAsync ()));
+					StartCentering ();
 				}
 			});
 		}
@@ -61,18 +61,13 @@ public class CenterOnChild : MonoBehaviour,IBeginDragHandler,IEndDragHandler
 	public void OnEndDrag (PointerEventData eventData)
 	{
 		if (center != null) {
-			centerCoroutines.Add (StartCoroutine (CenterAsync ()));
+			StartCentering ();
 		}
 	}
 
-	IEnumerator CenterAsync ()
+	void StartCentering ()
 	{
-		while (scrollView.velocity.x >= 2 * gridLayoutGroup.cellSize.x + gridLayoutGroup.spacing.x ||
-		       scrollView.velocity.y >= 2 * gridLayoutGroup.cellSize.y + gridLayoutGroup.spacing.y) {
-			yield return null;
-		}
-
-		var child = FindClosestChild (scrollView.velocity.normalized);
+		var child = FindClosestChild ();
 		centerCoroutines.Add (StartCoroutine (CenterAsync (child)));
 	}
 
@@ -99,13 +94,15 @@ public class CenterOnChild : MonoBehaviour,IBeginDragHandler,IEndDragHandler
 		centerCoroutines.Clear ();
 	}
 
-	Transform FindClosestChild (Vector2 direction)
+	Transform FindClosestChild ()
 	{
+		var direction = scrollView.velocity.normalized;
 		var childIndex = (scrollView.horizontal && direction.x > 0) || (scrollView.vertical && direction.y > 0) ? 0 : scrollView.content.childCount - 1;
 		var distance = Mathf.Infinity;
 
 		for (int i = 0; i < scrollView.content.childCount; i++) {
-			var pos = scrollView.content.GetChild (i).position;
+			var child = scrollView.content.GetChild (i);
+			var pos = child.position + child.TransformVector (new Vector3 (scrollView.velocity.x, scrollView.velocity.y, 0));
 			if (scrollView.horizontal) {
 				var dir = Vector3.Project (pos - center.position, center.right);
 				if (direction.x > 0 && dir.x > 0) {
