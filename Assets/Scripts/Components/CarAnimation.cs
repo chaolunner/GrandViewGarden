6922c993f07e644083c6f08f2b124b2baa0f6c19
tvvei <btvvei@gmail.com>
2017-08-13 14:48:56 +0800

    --add animation script

---
+++ Assets/Scripts/Components/CarAnimation.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using UnityEditor;
+using UnityEngine;
+
+public class CarAnimation : MonoBehaviour
+{
+    private Animator car;
+
+    public float rotation = -1;
+    public float direction = -1;
+
+    void Start()
+    {
+        car = gameObject.GetComponent<Animator>();
+
+    }
+    void Update()
+    {
+        if (car)
+        {
+            car.SetFloat("Direction", direction);
+            car.SetFloat("Rotation", rotation);
+        }
+        if (direction == -1)
+        {
+            rotation += 0.01f;
+            if (rotation >= 1)
+            {
+                direction = 1;
+            }
+        }
+        if (direction == 1)
+        {
+            rotation -= 0.01f;
+            if (rotation <= -1)
+            {
+                direction = -1;
+            }
+        }
+    }
+}
