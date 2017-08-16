5fc76eaf3efb43cc3ded97907ebc249f2e8ea812
chaolun <0chaolun0@gmail.com>
2017-08-16 23:12:25 +0800

    - add fps check utility

---
+++ Assets/Scripts/Editor/FPSUtilityDrawer.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEditor;
+using UnityEngine;
+using System.Linq;
+
+[CustomEditor (typeof(FPSUtility))]
+public class FPSUtilityDrawer : Editor
+{
+	public bool foldout;
+	public FPSUtility fpsUtility;
+	public List<FPSColor> fpsColorList = new List<FPSColor> ();
+
+	void OnEnable ()
+	{
+		fpsUtility = target as FPSUtility;
+	}
+
+	public override void OnInspectorGUI ()
+	{
+		EditorGUI.BeginChangeCheck ();
+		var frameRange = EditorGUILayout.IntField ("Frame Range", fpsUtility.frameRange);
+		var highestFPSLabel = (Text)EditorGUILayout.ObjectField ("Highest FPS Label", fpsUtility.highestFPSLabel, typeof(Text), true);
+		var averageFPSLabel = (Text)EditorGUILayout.ObjectField ("Average FPS Label", fpsUtility.averageFPSLabel, typeof(Text), true);
+		var lowestFPSLabel = (Text)EditorGUILayout.ObjectField ("Lowest FPS Label", fpsUtility.lowestFPSLabel, typeof(Text), true);
+
+		fpsColorList.Clear ();
+		fpsColorList.AddRange (fpsUtility.coloring);
+
+		foldout = EditorGUILayout.Foldout (foldout, "Edit Color Setting");
+		if (foldout) {
+			EditorGUI.indentLevel++;
+			for (int i = 0; i < fpsColorList.Count; i++) {
+				var fpsColor = fpsColorList [i];
+				var rect = EditorGUILayout.GetControlRect ();
+				rect = EditorGUI.PrefixLabel (rect, new GUIContent ("If FPS >= "));
+				rect.xMax -= 18;
+				var intRect = new Rect (rect);
+				var colRect = new Rect (rect);
+				var btnRect = new Rect (rect);
+				intRect.xMax = intRect.xMin + 0.5f * rect.size.x;
+				colRect.xMin = intRect.xMax;
+				btnRect.xMin = colRect.xMax;
+				btnRect.xMax = btnRect.xMin + 18;
+				fpsColor.minimumFPS = EditorGUI.IntField (intRect, fpsColor.minimumFPS);
+				fpsColor.color = EditorGUI.ColorField (colRect, fpsColor.color);
+				if (GUI.Button (btnRect, "-")) {
+					fpsColorList.RemoveAt (i);
+					break;
+				}
+				fpsColorList [i] = fpsColor;
+			}
+			
+			if (GUILayout.Button ("Add Color List")) {
+				var last = fpsColorList.LastOrDefault ();
+				var newest = new FPSColor ();
+				newest.minimumFPS = last.minimumFPS;
+				newest.color = last.color;
+				fpsColorList.Add (newest);
+			}
+			EditorGUI.indentLevel--;
+		}
+
+		if (EditorGUI.EndChangeCheck ()) {
+			Undo.RecordObject (fpsUtility, "Change FPS Setting");
+			fpsUtility.frameRange = frameRange;
+			fpsUtility.highestFPSLabel = highestFPSLabel;
+			fpsUtility.averageFPSLabel = averageFPSLabel;
+			fpsUtility.lowestFPSLabel = lowestFPSLabel;
+			fpsUtility.coloring = fpsColorList.ToArray ();
+			EditorUtility.SetDirty (fpsUtility);
+		}
+	}
+}
---
+++ Assets/Scripts/Utilities/FPSUtility.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+using System;
+
+[Serializable]
+public struct FPSColor
+{
+	public Color color;
+	public int minimumFPS;
+}
+
+public class FPSUtility : MonoBehaviour
+{
+	public int frameRange = 60;
+	public Text highestFPSLabel, averageFPSLabel, lowestFPSLabel;
+	public FPSColor[] coloring = new FPSColor[0];
+
+	int[] fpsBuffer;
+	int fpsBufferIndex;
+
+	public int AverageFPS { get; private set; }
+
+	public int HighestFPS { get; private set; }
+
+	public int LowestFPS { get; private set; }
+
+	void Start ()
+	{
+		InitializeBuffer ();
+		DontDestroyOnLoad (gameObject);
+	}
+
+	void Update ()
+	{
+		UpdateBuffer ();
+		CalculateFPS ();
+
+		Display (highestFPSLabel, HighestFPS);
+		Display (averageFPSLabel, AverageFPS);
+		Display (lowestFPSLabel, LowestFPS);
+	}
+
+	void InitializeBuffer ()
+	{
+		if (frameRange <= 0) {
+			frameRange = 1;
+		}
+		fpsBuffer = new int[frameRange];
+		fpsBufferIndex = 0;
+	}
+
+	void UpdateBuffer ()
+	{
+		fpsBuffer [fpsBufferIndex++] = (int)(1f / Time.unscaledDeltaTime);
+		if (fpsBufferIndex >= frameRange) {
+			fpsBufferIndex = 0;
+		}
+	}
+
+	void CalculateFPS ()
+	{
+		int sum = 0;
+		int highest = 0;
+		int lowest = int.MaxValue;
+		for (int i = 0; i < frameRange; i++) {
+			int fps = fpsBuffer [i];
+			sum += fps;
+			if (fps > highest) {
+				highest = fps;
+			}
+			if (fps < lowest) {
+				lowest = fps;
+			}
+		}
+		AverageFPS = (int)((float)sum / frameRange);
+		HighestFPS = highest;
+		LowestFPS = lowest;
+	}
+
+	void Display (Text label, int fps)
+	{
+		label.text = fps.ToString ();
+		for (int i = 0; i < coloring.Length; i++) {
+			if (fps >= coloring [i].minimumFPS) {
+				label.color = coloring [i].color;
+				break;
+			}
+		}
+	}
+}
