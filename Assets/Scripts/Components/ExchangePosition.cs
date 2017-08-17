bf240082f31f3719131f0c7d3e609a8f3cd13e06
chaolun <0chaolun0@gmail.com>
2017-08-18 00:34:35 +0800

    - update exchange position

--- Assets/Scripts/Components/ExchangePosition.cs
+++ Assets/Scripts/Components/ExchangePosition.cs
@@ -1,37 +1,52 @@
 ﻿using UnityEngine.EventSystems;
 using UniRx.Triggers;
 using UnityEngine;
+using System.Linq;
 using DG.Tweening;
 using UniRx;
 
 public class ExchangePosition : MonoBehaviour
 {
-	public Transform Origin;
-	public Transform Target;
+	public BoolReactiveProperty IsOn;
+	public Transform[] Origins;
+	public Transform[] Targets;
 	[Range (0, 1)]
 	public float Duration = 0.5f;
-
 	[Range (0, 2)]
 	public float Delay = 0;
 
 	void Start ()
 	{
-		gameObject.OnPointerClickAsObservable ().Subscribe (OnPointerClick);
+		IsOn.DistinctUntilChanged ().Where (b => b == true).Subscribe (_ => {
+			DoExchange ();
+		});
+
+		gameObject.OnPointerClickAsObservable ().Subscribe (_ => {
+			IsOn.Value = true;
+		});
+
+		foreach (var target in Targets) {
+			target.gameObject.SetActive (false);
+		}
 	}
 
-	void OnPointerClick (PointerEventData eventData)
+	void DoExchange ()
 	{
-		if (Origin == null || Target == null) {
+		if (Origins == null || Targets == null || Origins.Length <= 0 || Origins.Length != Targets.Length) {
 			return;
 		}
-		var originPosition = Origin.position;
-		var tweener = Origin.DOMove (Target.position, Duration);
-		tweener.SetDelay (Delay);
-		tweener.SetEase (Ease.InBack);
-		tweener.OnComplete (() => {
-			Target.gameObject.SetActive (true);
-			Origin.gameObject.SetActive (false);
-			Target.DOMove (originPosition, Duration).SetEase (Ease.OutBack);
-		});
+		for (int i = 0; i < Origins.Length; i++) {
+			var index = i;
+			var originPosition = Origins [index].position;
+			var tweener = Origins [index].DOMove (Targets [index].position, Duration);
+			tweener.SetDelay (Delay);
+			tweener.SetEase (Ease.InBack);
+			tweener.OnComplete (() => {
+				Targets [index].gameObject.SetActive (!Targets [index].gameObject.activeSelf);
+				Origins [index].gameObject.SetActive (!Origins [index].gameObject.activeSelf);
+				Targets [index].DOMove (originPosition, Duration).SetEase (Ease.OutBack);
+				IsOn.Value = false;
+			});
+		}
 	}
 }
