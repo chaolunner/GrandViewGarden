6baf362e77bd9e2d1d4e8399d35d675d93aedb43
tvvei <btvvei@gmail.com>
2017-08-18 00:11:01 +0800

    -add Pass scene

---
+++ Assets/CarWheelCollider.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class AxleInfo{
+	public WheelCollider leftWheel;
+	public WheelCollider rightWheel;
+	public bool motor;
+	public bool steering;
+}
+public class CarWheelCollider : MonoBehaviour {
+	public List<AxleInfo> axleInfos;
+	public float maxMotorTorque;
+	public float maxSteeringAngle;
+	[Range (0,9)]
+	public float speed=5;
+
+	public void ApplyLocalPositionToVisuals(WheelCollider collider){
+		if (collider.transform.childCount == 0) {
+			return;
+		}
+		Transform visualWheel = collider.transform.GetChild (0);
+
+		Vector3 position;
+		Quaternion rotation;
+		collider.GetWorldPose (out position, out rotation);
+
+		visualWheel.transform.position = position;
+		visualWheel.transform.rotation = rotation;
+	}
+
+	void FixedUpdate(){		
+		float motor = speed*20 + maxMotorTorque *Input.GetAxis("Vertical");
+		float steering = maxSteeringAngle * Input.GetAxis ("Horizontal");
+
+		foreach (AxleInfo axleInfo in axleInfos) {
+			if (axleInfo.steering) {
+				axleInfo.leftWheel.steerAngle = steering;
+				axleInfo.rightWheel.steerAngle = steering;
+			}
+			if (axleInfo.motor) {
+				axleInfo.leftWheel.motorTorque = motor;
+				axleInfo.rightWheel.motorTorque = motor;
+			}
+			ApplyLocalPositionToVisuals (axleInfo.leftWheel);
+			ApplyLocalPositionToVisuals (axleInfo.rightWheel);
+		}
+	}
+}
---
+++ Assets/FollowCamera.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour {
+	public Transform Car;
+	public float speed = 5f;
+	Vector3 offset;
+
+	void Start () {		
+		offset = transform.position-Car.position  ;
+	}
+
+	void Update () {
+		Vector3 targetCameraPosition = Car.position + offset;
+		transform.position = Vector3.Lerp (transform.position, targetCameraPosition, speed * Time.deltaTime);
+	}
+}
