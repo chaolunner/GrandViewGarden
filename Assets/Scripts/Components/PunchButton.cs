e8571b11fae90c65739feb87eb237d3468ed57ed
chaolun <0chaolun0@gmail.com>
2017-08-18 23:50:20 +0800

    - remove no used logic

--- Assets/Scripts/Components/PunchButton.cs
+++ Assets/Scripts/Components/PunchButton.cs
@@ -1,9 +1,5 @@
-﻿//using UnityEngine.EventSystems;
-//using UniRx.Triggers;
-using UnityEngine;
-//using DG.Tweening;
+﻿using UnityEngine;
 using UniECS;
-//using UniRx;
 
 public class PunchButton : ComponentBehaviour
 {
@@ -11,18 +7,4 @@ public class PunchButton : ComponentBehaviour
 	public float Scale = 1.2f;
 	[Range (0, 1)]
 	public float Duration = 0.2f;
-
-//	void Start ()
-//	{
-//		gameObject.OnPointerDownAsObservable ().Subscribe (PointerDown);
-//	}
-//
-//	void PointerDown (PointerEventData eventData)
-//	{
-//		var originScale = transform.localScale;
-//		var tweener = transform.DOScale (Scale * Vector2.one, Duration);
-//		tweener.OnComplete (() => {
-//			transform.DOScale (originScale, Duration);
-//		});
-//	}
 }
