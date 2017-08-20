800bf32107c3e3ba62d1bc0b5bee6b8a088259e8
chaolun <0chaolun0@gmail.com>
2017-08-20 20:07:25 +0800

    - optimize time system

---
+++ Assets/Scripts/Components/Timing.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UniECS;
+
+public class Timing : ComponentBehaviour
+{
+	public int Hour;
+	public int Minute;
+	public int Second;
+	public int MilliSecond;
+	[HideInInspector]
+	public float TimeSpeed = 0.0f;
+}
