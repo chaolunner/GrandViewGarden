9a5d24b84a0474fe2ca6a74fd6dce26b91999e53
chaolun <0chaolun0@gmail.com>
2017-08-23 01:40:17 +0800

    - update follow camera system

---
+++ Assets/Scripts/Components/FollowCamera.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UniECS;
+
+public class FollowCamera : ComponentBehaviour
+{
+	public Transform Target;
+	public AnimationCurve Speed = new AnimationCurve (new Keyframe[] {
+		new Keyframe (0, 1),
+		new Keyframe (1, 1),
+	});
+}
