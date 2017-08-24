1af7facb46359e37c9704591d783f0e15a4a2a3d
chaolun <0chaolun0@gmail.com>
2017-08-25 00:22:36 +0800

    - update doTween - update pause vfx system

---
+++ Assets/Scripts/TODO/GamePanel.cs
@@ -0,0 +1,6 @@
+﻿using UnityEngine;
+using UniECS;
+
+public class GamePanel : ComponentBehaviour
+{
+}
---
+++ Assets/Scripts/TODO/PauseVFXSystem.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using DG.Tweening;
+using System;
+using UniECS;
+using UniRx;
+
+public class PauseVFXSystem : SystemBehaviour
+{
+	public override void Awake ()
+	{
+		base.Awake ();
+
+		var FollowCameraEntities = GroupFactory.Create (new Type[] {
+			typeof(FollowCamera),
+			typeof(Camera),
+		});
+
+		var PausePanelEntities = GroupFactory.Create (new Type[] {
+			typeof(PausePanel)
+		});
+
+		var GamePanelEntities = GroupFactory.Create (new Type[] {
+			typeof(GamePanel)
+		});
+
+		Observable.CombineLatest (FollowCameraEntities.OnAdd (), PausePanelEntities.OnAdd (), GamePanelEntities.OnAdd (), (followCameraEntity, pausePanelEntity, gamePanelEntity) => {
+			var camera = followCameraEntity.GetComponent<Camera> ();
+			var canvasGroup = pausePanelEntity.GetComponent<CanvasGroup> ();
+			var gamePanel = gamePanelEntity.GetComponent<GamePanel> ();
+
+			var originFieldOfView = camera.fieldOfView;
+			var originAlpha = canvasGroup.alpha;
+			var originPosition = gamePanel.transform.position;
+
+			Sequence sequence = null;
+
+			EventSystem.OnEvent<GamePause> ().Subscribe (_ => {
+				sequence.Kill ();
+				sequence = DOTween.Sequence ()
+					.Join (camera.DOFieldOfView (30, 1).SetRelative ())
+					.Join (canvasGroup.DOFade (1, 1).SetRelative ())
+					.Join (gamePanel.transform.DOMoveY (originPosition.y + 500, 1))
+					.SetUpdate (true);
+			});
+
+			EventSystem.OnEvent<GamePlay> ().Subscribe (_ => {
+				sequence.Kill ();
+				sequence = DOTween.Sequence ()
+					.Join (camera.DOFieldOfView (originFieldOfView, 1))
+					.Join (canvasGroup.DOFade (originAlpha, 1))
+					.Join (gamePanel.transform.DOMove (originPosition, 1))
+					.SetUpdate (true);
+			});
+
+			return true;
+		}).Subscribe ().AddTo (this.Disposer);
+	}
+}
